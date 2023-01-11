9ce371b14893860c71042c870a375f16b4c2219e
Rahul Desar <desarrahul@gmail.com>
2023-01-11 16:40:17 +0545

    initialize CRA

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import TableProduct from "./components/TableProduct";
 
-function App() {
+const App = () => {
+  let [data, setData] = useState([]);
+
+  useEffect(() => {
+    let baseUrl = "https://product-fhqo.onrender.com/products";
+
+    fetch(baseUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data.products);
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  console.log(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="items-center">
+      <h2> Task 2 : CRUD Operation </h2>
+      <div></div>
+
+      <TableProduct data={data} />
     </div>
   );
-}
+};
 
 export default App;
