55b021a2547bd94665c51286dddf21cd0a07a004
Rahul Desar <desarrahul@gmail.com>
2023-01-12 10:20:35 +0545

    Filter Refactored. + solved task queue issue

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,32 +1,38 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Filter = ({ data, setFilteredData }) => {
   const [newFilter, setNewFilter] = useState("");
-  console.log(newFilter);
 
-  const dataToShow =
-    newFilter.length === 0
-      ? data
-      : data.filter(
-          (singleProduct) =>
-            singleProduct.product_name
-              .toLowerCase()
-              .includes(newFilter.toLowerCase()) ||
-            singleProduct.category_name
-              .toLowerCase()
-              .includes(newFilter.toLowerCase())
-        );
+  // * Filtering Logic and Updates the data to show.
+  let filterDataEffect = () => {
+    const dataToShow =
+      newFilter.length === 0
+        ? data
+        : data.filter(
+            (singleProduct) =>
+              singleProduct.product_name
+                .toLowerCase()
+                .includes(newFilter.toLowerCase()) ||
+              singleProduct.category_name
+                .toLowerCase()
+                .includes(newFilter.toLowerCase())
+          );
 
-  const handleFilterChange = (e) => {
-    setNewFilter(e.target.value);
     setFilteredData(dataToShow);
   };
 
+  useEffect(filterDataEffect, [newFilter, data, setFilteredData]);
+
+  const handleFilterChange = (e) => {
+    setNewFilter(e.target.value);
+  };
+
   return (
-    <div>
-      filter shown with:
-      <input onChange={handleFilterChange} />
-    </div>
+    <input
+      className="bg-gray-200 border-gray-200 border-2 focus:outline-none focus:bg-white focus:border-emerald-400 rounded px-6 py-2"
+      placeholder="Search"
+      onChange={handleFilterChange}
+    />
   );
 };
 
